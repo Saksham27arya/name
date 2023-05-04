13f90d15ebe6de82e907e5db079739c328334cd1
Saksham Arya <sakshamarya2001nagal@gmail.com>
2023-05-04 23:53:10 +0530

    Add files via upload

---
+++ Name.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ") 
+current_age = int(input("Enter your age: "))
+hundredth_year = 2020 + (100 - current_age)
+print(f'{name} will become 100 years old in the year {hundredth_year}.')
